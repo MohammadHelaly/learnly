4a9b2903ad028ee4dfb660bf2a86604751e5f5ab
Shamarka <shamarka2001@gmail.com>
2024-03-08 22:03:47 +0200

    Fixing channel deletion middleware --comments--

--- server/models/channelModel.js
+++ server/models/channelModel.js
@@ -3,128 +3,177 @@ const Course = require("./courseModel");
 const Message = require("./messageModel");
 
 const channelSchema = mongoose.Schema(
-	{
-		name: { type: String, trim: true },
-		course: {
-			type: mongoose.Schema.ObjectId,
-			ref: "Course",
-		},
-		isCourseChannel: { type: Boolean, default: true, required: true },
-		users: [{ type: mongoose.Schema.ObjectId, ref: "User" }],
-		latestMessage: {
-			type: mongoose.Schema.ObjectId,
-			ref: "Message",
-		},
-		admins: [{ type: mongoose.Schema.ObjectId, ref: "User" }],
-	},
-	{
-		timestamps: true,
-		toJSON: { virtuals: true },
-		toObject: { virtuals: true },
-	}
+  {
+    name: { type: String, trim: true },
+    course: {
+      type: mongoose.Schema.ObjectId,
+      ref: "Course",
+    },
+    isCourseChannel: { type: Boolean, default: true, required: true },
+    users: [{ type: mongoose.Schema.ObjectId, ref: "User" }],
+    latestMessage: {
+      type: mongoose.Schema.ObjectId,
+      ref: "Message",
+    },
+    admins: [{ type: mongoose.Schema.ObjectId, ref: "User" }],
+  },
+  {
+    timestamps: true,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
 );
 
 // Virtual populate
 channelSchema.virtual("messages", {
-	ref: "Message",
-	foreignField: "channel",
-	localField: "_id",
+  ref: "Message",
+  foreignField: "channel",
+  localField: "_id",
 });
 
 // DOCUMENT MIDDLEWARE: runs before .save() and .create() but not .insertMany() or .update(), etc
 channelSchema.pre("save", async function (next) {
-	if (this.isNew) {
-		if (!this.isCourseChannel) {
-			return next();
-		}
+  if (this.isNew) {
+    if (!this.isCourseChannel) {
+      return next();
+    }
 
-		const courseId = this.course;
+    const courseId = this.course;
 
-		const existingChannel = await Channel.findOne({ course: courseId });
+    const existingChannel = await Channel.findOne({ course: courseId });
 
-		if (existingChannel) {
-			throw new Error(
-				`Channel for course with id ${courseId} already exists.`
-			);
-		}
+    if (existingChannel) {
+      throw new Error(`Channel for course with id ${courseId} already exists.`);
+    }
 
-		try {
-			const course = await Course.findByIdAndUpdate(
-				courseId,
-				{ $set: { channel: this._id } },
-				{ new: true }
-			);
+    try {
+      const course = await Course.findByIdAndUpdate(
+        courseId,
+        { $set: { channel: this._id } },
+        { new: true }
+      );
 
-			if (!course) {
-				throw new Error(`Course with id ${courseId} not found.`);
-			}
+      if (!course) {
+        throw new Error(`Course with id ${courseId} not found.`);
+      }
 
-			this.name = course.name;
-			this.admins = course.instructors;
-			this.users = course.instructors; // TODO: check if this is the right approach
-		} catch (error) {
-			console.error(
-				`Error occurred while updating course with id ${courseId}.`
-			);
+      this.name = course.name;
+      this.admins = course.instructors;
+      this.users = course.instructors; // TODO: check if this is the right approach
+    } catch (error) {
+      console.error(
+        `Error occurred while updating course with id ${courseId}.`
+      );
 
-			return next(error);
-		}
-	}
-	next();
+      return next(error);
+    }
+  }
+  next();
 });
 
 // pre delete middleware is not working, need to fix it
-channelSchema.pre(/^delete/, async function (next) {
-	if (!this.isCourseChannel) {
-		return next();
-	}
+channelSchema.pre(/^delete/ /*deleteOne OR remove*/, async function (next) {
+  //instead of /^delete , try "deleteOne" for channel and "deleteMany" for messages
+  if (!this.isCourseChannel) {
+    return next();
+  }
 
-	const courseId = this.course;
+  const courseId = this.course;
 
-	try {
-		const course = await Course.findByIdAndUpdate(
-			courseId,
-			{
-				$unset: { channel: "" },
-			},
-			{ new: true }
-		);
+  try {
+    const course = await Course.findByIdAndUpdate(
+      courseId,
+      {
+        $unset: { channel: "" },
+      },
+      { new: true }
+    );
 
-		if (!course) {
-			throw new Error(`Course with id ${courseId} not found.`);
-		}
-	} catch (error) {
-		console.error(
-			`Error occurred while updating course with id ${courseId}.`
-		);
+    if (!course) {
+      throw new Error(`Course with id ${courseId} not found.`);
+    }
+  } catch (error) {
+    console.error(`Error occurred while updating course with id ${courseId}.`);
 
-		return next(error);
-	}
-	next();
+    return next(error);
+  }
+  next();
 });
 
-channelSchema.pre(/^delete/, async function (next) {
-	const channelId = this._id;
+//-------------------------------fixing the channel and messages deletion middleware----------------------------------
 
-	try {
-		const messages = await Message.deleteMany({ channel: channelId });
+// channelSchema.pre("deleteOne", async function (next) {
+//     if (!this.isCourseChannel) {
+//         return next();
+//     }
 
-		if (!messages) {
-			throw new Error(
-				`Messages for channel with id ${channelId} not found.`
-			);
-		}
+//     const courseId = this.course;
 
-		console.log(`Messages for channel with id ${channelId} deleted.`);
-	} catch (error) {
-		console.error(
-			`Error occurred while deleting messages for channel with id ${channelId}.`
-		);
+//     try {
+//         const course = await Course.findByIdAndUpdate(
+//             courseId,
+//             {
+//                 $unset: { channel: "" },
+//             },
+//             { new: true }
+//         );
 
-		return next(error);
-	}
-	next();
+//         if (!course) {
+//             throw new Error(`Course with id ${courseId} not found.`);
+//         }
+//     } catch (error) {
+//         console.error(
+//             `Error occurred while updating course with id ${courseId}: ${error.message}`
+//         );
+
+//         return next(error);
+//     }
+//     next();
+// });
+
+//---------------------------------------------------------------------------------------------------------------------------------
+
+channelSchema.pre(/^delete/ /*deleteMany*/, async function (next) {
+  const channelId = this._id;
+
+  try {
+    const messages = await Message.deleteMany({ channel: channelId });
+
+    if (!messages) {
+      throw new Error(`Messages for channel with id ${channelId} not found.`);
+    }
+
+    console.log(`Messages for channel with id ${channelId} deleted.`);
+  } catch (error) {
+    console.error(
+      `Error occurred while deleting messages for channel with id ${channelId}.`
+    );
+
+    return next(error);
+  }
+  next();
 });
+
+// channelSchema.pre("deleteMany", async function (next) {
+// 	const channelId = this._id;
+
+// 	try {
+// 	  const messages = await Message.deleteMany({ channel: channelId });
+
+// 	  if (!messages) {
+// 		throw new Error(`Messages for channel with id ${channelId} not found.`);
+// 	  }
+
+// 	  console.log(`Messages for channel with id ${channelId} deleted.`);
+// 	} catch (error) {
+// 	  console.error(
+// 		`Error occurred while deleting messages for channel with id ${channelId}.`
+// 	  );
+
+// 	  return next(error);
+// 	}
+// 	next();
+//   });
 
 const Channel = mongoose.model("Channel", channelSchema);
 
