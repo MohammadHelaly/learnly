ee016f0e1c843a5cdf0f69ae8979930574f0a99b
Moustafa Daebis <mostafa.dabis@gmail.com>
2024-04-19 23:18:46 +0200

    Minor Modification to the Student Course Page Video

--- client/src/components/CourseStudentDashboardPage/CourseStudentContent.tsx
+++ client/src/components/CourseStudentDashboardPage/CourseStudentContent.tsx
@@ -247,7 +247,7 @@ const CourseStudentContents = (props: CourseContentsProps) => {
 							controls
 							className="MuiCardMedia-media"
 							sx={{
-								width: "100%",
+								width: open ? "85%" : "100%",
 								height: "100%",
 								minHeight: "100%",
 							}}
