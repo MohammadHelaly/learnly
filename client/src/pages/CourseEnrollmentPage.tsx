7d7136eaa9b92b666280efd53d84f975a6ab4c6e
Moustafa Daebis <mostafa.dabis@gmail.com>
2024-04-25 17:44:25 +0200

    Update CourseEnrollmentPage.tsx

--- client/src/pages/CourseEnrollmentPage.tsx
+++ client/src/pages/CourseEnrollmentPage.tsx
@@ -4,15 +4,24 @@ import { useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import { useQueryClient, useMutation } from "@tanstack/react-query";
 import AuthContext from "../store/auth-context";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import PageWrapper from "../components/UI/PageLayout/PageWrapper";
 import api from "../api";
 import SectionHeader from "../components/UI/PageLayout/SectionHeader";
 import FormContainer from "../components/UI/PageLayout/FormContainer";
-import { Button, Container, Typography } from "@mui/material";
+import CourseSelection from "../components/UI/Courses/Catalog/CourseSelection";
+import dummyCoursesData from "../assets/data/dummyCoursesData";
+import {
+	Button,
+	Container,
+	Typography,
+	Stack,
+	Rating,
+	Box,
+} from "@mui/material";
 import Popup from "../components/Popup/Popup";
 import { useNavigate } from "react-router-dom";
-import CourseImage from "../components/UI/Courses/Catalog/CourseImage";
+import SectionWrapper from "../components/UI/PageLayout/SectionWrapper";
 
 function CourseEnrollmentPage() {
 	const { courseId } = useParams();
@@ -23,13 +32,16 @@ function CourseEnrollmentPage() {
 		navigate("/dashboard");
 	};
 
+	const dummyCourse = dummyCoursesData.find(
+		(course) => course.id === (courseId as string)
+	);
+
 	const { data, isLoading, isError } = useQuery({
 		queryKey: ["courses", { courseId }],
 		queryFn: async () => await api.get(`/courses/${courseId}`),
 		select: (response) => response.data.data.data,
 	});
-	const course = data;
-	console.log(course);
+	const course = data ?? dummyCourse;
 
 	const {
 		mutate: mutateUser,
@@ -58,39 +70,166 @@ function CourseEnrollmentPage() {
 
 	return (
 		<AnimatedPage>
-			<PageWrapper centered>
-				<FormContainer>
-					<Typography
-						variant="h4"
-						color="common.black"
-						align="center"
-					>
-						Course Enrollment
-					</Typography>
-					<Container sx={{ pt: 1, pd: 1 }}>
-						<CourseImage
-							imageCover={course?.imageCover}
-							name={course?.name}
-							isLoading={isLoading}
-						/>
-						<Typography>{course?.name}</Typography>
-					</Container>
-					<Button
-						variant="contained"
-						color="primary"
-						size="large"
-						disableElevation
-						disabled={isPendingUser}
-						sx={{ width: "100%", mt: 2 }}
-						onClick={() => {
-							if (authContext.user) {
-								mutateUser(authContext.user.id);
-							}
+			<PageWrapper>
+				{!isLoading ? (
+					<SectionWrapper>
+						<Container>
+							<Stack display="flex" flexDirection="row">
+								<Container sx={{ width: "80%" }}>
+									<Stack>
+										<SectionHeader
+											heading="Enrollment"
+											headingAlignment="left"
+											keepHeadingAlignmentOnSmallScreens
+											headingAnimated={false}
+											sx={{
+												mb: 0,
+												pb: 4,
+											}}
+										/>
+
+										<Typography
+											variant="body1"
+											fontWeight="light bold"
+											sx={{
+												mb: 2,
+												borderBottom:
+													"1px solid #e0e0e0",
+												paddingBottom: 1,
+											}}
+										>
+											Course
+										</Typography>
+										<Stack
+											direction="row"
+											justifyContent="space-between"
+										>
+											<Stack direction="row" spacing={3}>
+												<img
+													src={course?.imageCover.url}
+													alt={course?.name}
+												/>
+												<Stack direction="column">
+													<Typography fontWeight="bold">
+														{course?.name}
+													</Typography>
+													<Typography
+														fontWeight="light bold"
+														variant="body2"
+													>
+														By{" "}
+														{
+															course
+																?.instructors[0]
+																.name
+														}
+													</Typography>
+													<Stack
+														direction="row"
+														spacing={1}
+													>
+														<Typography
+															color="secondary.main"
+															sx={{ pt: 0.3 }}
+														>
+															{
+																course?.ratingsAverage
+															}
+														</Typography>
+														<Rating
+															name="read-only"
+															value={
+																course?.ratingsAverage
+															}
+															readOnly
+															precision={0.25}
+															size="small"
+															sx={{
+																color: "secondary.main",
+																pt: 0.6,
+															}}
+														/>
+													</Stack>
+													<Typography
+														fontWeight="1"
+														variant="body2"
+													>
+														{course.duration} Hours
+													</Typography>
+													<Typography
+														fontWeight="1"
+														variant="body2"
+													>
+														{course.difficulty}
+													</Typography>
+												</Stack>
+											</Stack>
+											<Typography>
+												E£{course.price}
+											</Typography>
+										</Stack>
+									</Stack>
+								</Container>
+
+								<Box
+									sx={{
+										borderBottom: "1px solid #e0e0e0",
+										pt: 4,
+										mt: 2,
+									}}
+								>
+									<Typography sx={{ pt: 4 }}>
+										Total:
+									</Typography>
+									<Typography
+										sx={{ pt: 1, pb: 4, mb: 4 }}
+										variant="h4"
+										fontWeight="light bold"
+									>
+										E£{course.price}
+									</Typography>
+									<Button
+										variant="contained"
+										color="primary"
+										size="large"
+										disableElevation
+										disabled={isPendingUser}
+										sx={{
+											width: "100%",
+											mt: 2,
+										}}
+										onClick={() => {
+											if (authContext.user) {
+												mutateUser(authContext.user.id);
+											}
+										}}
+									>
+										Enroll in Course
+									</Button>
+								</Box>
+							</Stack>
+						</Container>
+					</SectionWrapper>
+				) : (
+					<Typography>isLoading</Typography>
+				)}
+				<Container>
+					<CourseSelection
+						heading="See Some Similar Courses"
+						headingAlignment="left"
+						headingAnimated={false}
+						variant="white"
+						query={{
+							url: "/courses",
+							config: {
+								params: {
+									categories: { in: course?.categories }, // Look into adding $ here instead of in regex in the backend
+									_id: { ne: courseId },
+								},
+							},
 						}}
-					>
-						Enroll in Course
-					</Button>
-				</FormContainer>
+					/>
+				</Container>
 			</PageWrapper>
 
 			<Footer />
