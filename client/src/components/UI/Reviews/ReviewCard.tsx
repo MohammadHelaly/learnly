0de6291bab48028e52b7ce139a8244b948dbf78b
Moustafa Daebis <mostafa.dabis@gmail.com>
2024-05-01 14:33:18 +0300

    Add Edit review Menu

--- client/src/components/UI/Reviews/ReviewCard.tsx
+++ client/src/components/UI/Reviews/ReviewCard.tsx
@@ -1,6 +1,9 @@
 import { Card, Stack, Typography, Rating, Avatar } from "@mui/material";
-
+import ReviewOptions from "./ReviewOptions";
+import AuthContext from "../../../store/auth-context";
+import { useContext } from "react";
 const ReviewCard = (props: Review) => {
+	const authContext = useContext(AuthContext);
 	const { rating, review, createdAt, user } = props;
 	const { name, photo } = user;
 
@@ -13,19 +16,22 @@ const ReviewCard = (props: Review) => {
 				borderBottom: 1,
 				borderRadius: 0,
 				borderColor: "divider",
-			}}>
+			}}
+		>
 			<Stack
 				direction="row"
 				spacing={2}
 				alignItems="center"
 				sx={{
 					mb: 1,
-				}}>
+				}}
+			>
 				<Avatar alt={name} src={photo?.url} />
 				<Typography variant="h6">{name}</Typography>
 				<Typography variant="body2" color="text.secondary">
 					{new Date(createdAt).toLocaleDateString()}
 				</Typography>
+				{authContext.user?.id === user.id && <ReviewOptions />}
 			</Stack>
 			<Rating
 				name="read-only"
@@ -42,7 +48,8 @@ const ReviewCard = (props: Review) => {
 				color="text.secondary"
 				sx={{
 					mb: 2,
-				}}>
+				}}
+			>
 				{review}
 			</Typography>
 		</Card>
