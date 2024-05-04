5019a5207835f79812474e7fa0dce1cbf0cdcd83
Moustafa Daebis <mostafa.dabis@gmail.com>
2024-05-04 20:35:49 +0300

    Apply course Enrollment middleware

--- server/controllers/reviewController.js
+++ server/controllers/reviewController.js
@@ -5,37 +5,40 @@ const AppError = require("../utils/appError");
 const handlerFactory = require("./handlerFactory");
 const Course = require("../models/courseModel");
 const User = require("../models/userModel");
+const CourseEnrollment = require("../models/courseEnrollmentModel");
 
 exports.setCourseUserIds = (req, res, next) => {
-  // Allow nested routes
-  if (!req.body.course) req.body.course = req.params.courseId;
-  if (!req.body.user) req.body.user = req.user.id;
-  next();
+	// Allow nested routes
+	if (!req.body.course) req.body.course = req.params.courseId;
+	if (!req.body.user) req.body.user = req.user.id;
+	next();
 };
 
 exports.protectReview = async (req, res, next) => {
-  const review = await Review.findById(req.params.id);
-  if (review.user.id !== req.user.id && req.user.role !== "admin") {
-    return next(
-      new AppError("You are not authorized to perform this action.", 403)
-    );
-  }
-  next();
+	const review = await Review.findById(req.params.id);
+	if (review.user.id !== req.user.id && req.user.role !== "admin") {
+		return next(
+			new AppError("You are not authorized to perform this action.", 403)
+		);
+	}
+	next();
 };
 
 //TODO: check if user is enrolled in course before creating review
 
 exports.checkEnrollment = catchAsync(async (req, res, next) => {
-  const courseId = req.body.course;
-  const userId = req.user.id;
-  const enrolledCourse = await Course.findOne({
-    _id: courseId,
-    students: userId,
-  });
-  if (!enrolledCourse) {
-    return next(new AppError("You are not enrolled in this course.", 403));
-  }
-  next();
+	const courseId = req.body.course;
+	const userId = req.user.id;
+	const enrolledCourse = await CourseEnrollment.findOne({
+		course: courseId,
+		user: userId,
+	});
+	if (!enrolledCourse) {
+		return next(
+			new AppError("You are not authorized to perform this action", 403)
+		);
+	}
+	next();
 });
 
 exports.getAllReviews = handlerFactory.getAll(Review);
--- server/routes/reviewRoutes.js
+++ server/routes/reviewRoutes.js
@@ -5,24 +5,25 @@ const authController = require("../controllers/authController");
 const router = express.Router({ mergeParams: true });
 
 router.route("/").get(reviewController.getAllReviews).post(
-  authController.protect,
-  //TODO: Middleware to check if user is enrolled in course before adding review
-  reviewController.setCourseUserIds,
-  reviewController.createReview
+	authController.protect,
+	//TODO: Middleware to check if user is enrolled in course before adding review
+	reviewController.checkEnrollment,
+	reviewController.setCourseUserIds,
+	reviewController.createReview
 );
 
 router
-  .route("/:id")
-  .get(reviewController.getReview)
-  .patch(
-    authController.protect,
-    reviewController.protectReview,
-    reviewController.updateReview
-  )
-  .delete(
-    authController.protect,
-    reviewController.protectReview,
-    reviewController.deleteReview
-  );
+	.route("/:id")
+	.get(reviewController.getReview)
+	.patch(
+		authController.protect,
+		reviewController.protectReview,
+		reviewController.updateReview
+	)
+	.delete(
+		authController.protect,
+		reviewController.protectReview,
+		reviewController.deleteReview
+	);
 
 module.exports = router;
