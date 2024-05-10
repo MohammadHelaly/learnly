3da6583abea13c041e92e5015ead01e549d60c1c
Moustafa Daebis <moustafa.daebis@outlook.com>
2024-05-10 03:35:01 +0300

    Add team member description

--- client/src/assets/data/teamMembers.ts
+++ client/src/assets/data/teamMembers.ts
@@ -1,4 +1,5 @@
 import mohammadHelaly from "../images/mohammad-helaly.jpg";
+import moustafaDaebis from "../images/moustafaDaebis.jpg";
 
 const teamMembers: TeamMember[] = [
 	{
@@ -12,14 +13,14 @@ const teamMembers: TeamMember[] = [
 		github: "https://wwww.github.com/MohammadHelaly",
 	},
 	{
-		name: "Mohammad Helaly",
+		name: "Moustafa Daebis",
 		role: "Full Stack Developer",
-		photo: mohammadHelaly,
+		photo: moustafaDaebis,
 		description:
-			"The creative mind behind our platform. He combined his technical frontend and backend expertise to build Learnly and empower students and instructors globally. Passionate about quality education, he's committed to making learning engaging for all.",
-		email: "mohammad.helaly@gmail.com",
-		linkedIn: "https://www.linkedin.com/in/mohammadhelaly/",
-		github: "https://wwww.github.com/MohammadHelaly",
+			"The technical genius behind our platform. He leveraged his expertise in frontend and backend development to create Learnly and provide a seamless learning experience for students and instructors worldwide.He's dedicated to making learning accessible to all.",
+		email: "moustafa.daebis@outlook.com",
+		linkedIn: "https://www.linkedin.com/in/moustafa-daebis-3a302b216/",
+		github: "https://github.com/Moustafa-Daebis",
 	},
 	{
 		name: "Mohammad Helaly",
