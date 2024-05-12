fe116adc3132297f9d14e742e572ee9a92e4d0fe
Shamarka <shamarka2001@gmail.com>
2024-05-12 20:55:12 +0300

    added team member info

--- client/src/assets/data/teamMembers.ts
+++ client/src/assets/data/teamMembers.ts
@@ -1,67 +1,67 @@
 import mohammadHelaly from "../images/mohammad-helaly.jpg";
 import moustafaDaebis from "../images/moustafaDaebis.jpg";
-
+import Shamarka from "../images/shamarka.jpg";
 const teamMembers: TeamMember[] = [
-	{
-		name: "Mohammad Helaly",
-		role: "Full Stack Developer",
-		photo: mohammadHelaly,
-		description:
-			"The creative mind behind our platform. He combined his technical frontend and backend expertise to build Learnly and empower students and instructors globally. Passionate about quality education, he's committed to making learning engaging for all.",
-		email: "mohammad.helaly@gmail.com",
-		linkedIn: "https://www.linkedin.com/in/mohammadhelaly/",
-		github: "https://wwww.github.com/MohammadHelaly",
-	},
-	{
-		name: "Moustafa Daebis",
-		role: "Full Stack Developer",
-		photo: moustafaDaebis,
-		description:
-			"The technical architect behind our platform. He contributed to both the frontend and backend development of Learnly, ensuring a seamless user experience. He's passionate about building scalable applications that make a difference.",
-		email: "moustafa.daebis@outlook.com",
-		linkedIn: "https://www.linkedin.com/in/moustafa-daebis-3a302b216/",
-		github: "https://github.com/Moustafa-Daebis",
-	},
-	{
-		name: "Mohammad Helaly",
-		role: "Full Stack Developer",
-		photo: mohammadHelaly,
-		description:
-			"The creative mind behind our platform. He combined his technical frontend and backend expertise to build Learnly and empower students and instructors globally. Passionate about quality education, he's committed to making learning engaging for all.",
-		email: "mohammad.helaly@gmail.com",
-		linkedIn: "https://www.linkedin.com/in/mohammadhelaly/",
-		github: "https://wwww.github.com/MohammadHelaly",
-	},
-	{
-		name: "Mohammad Helaly",
-		role: "Full Stack Developer",
-		photo: mohammadHelaly,
-		description:
-			"The creative mind behind our platform. He combined his technical frontend and backend expertise to build Learnly and empower students and instructors globally. Passionate about quality education, he's committed to making learning engaging for all.",
-		email: "mohammad.helaly@gmail.com",
-		linkedIn: "https://www.linkedin.com/in/mohammadhelaly/",
-		github: "https://wwww.github.com/MohammadHelaly",
-	},
-	{
-		name: "Mohammad Helaly",
-		role: "Full Stack Developer",
-		photo: mohammadHelaly,
-		description:
-			"The creative mind behind our platform. He combined his technical frontend and backend expertise to build Learnly and empower students and instructors globally. Passionate about quality education, he's committed to making learning engaging for all.",
-		email: "mohammad.helaly@gmail.com",
-		linkedIn: "https://www.linkedin.com/in/mohammadhelaly/",
-		github: "https://wwww.github.com/MohammadHelaly",
-	},
-	{
-		name: "Mohammad Helaly",
-		role: "Full Stack Developer",
-		photo: mohammadHelaly,
-		description:
-			"The creative mind behind our platform. He combined his technical frontend and backend expertise to build Learnly and empower students and instructors globally. Passionate about quality education, he's committed to making learning engaging for all.",
-		email: "mohammad.helaly@gmail.com",
-		linkedIn: "https://www.linkedin.com/in/mohammadhelaly/",
-		github: "https://wwww.github.com/MohammadHelaly",
-	},
+  {
+    name: "Mohammad Helaly",
+    role: "Full Stack Developer",
+    photo: mohammadHelaly,
+    description:
+      "The creative mind behind our platform. He combined his technical frontend and backend expertise to build Learnly and empower students and instructors globally. Passionate about quality education, he's committed to making learning engaging for all.",
+    email: "mohammad.helaly@gmail.com",
+    linkedIn: "https://www.linkedin.com/in/mohammadhelaly/",
+    github: "https://wwww.github.com/MohammadHelaly",
+  },
+  {
+    name: "Moustafa Daebis",
+    role: "Full Stack Developer",
+    photo: moustafaDaebis,
+    description:
+      "The technical architect behind our platform. He contributed to both the frontend and backend development of Learnly, ensuring a seamless user experience. He's passionate about building scalable applications that make a difference.",
+    email: "moustafa.daebis@outlook.com",
+    linkedIn: "https://www.linkedin.com/in/moustafa-daebis-3a302b216/",
+    github: "https://github.com/Moustafa-Daebis",
+  },
+  {
+    name: "Mohamed Shamarka",
+    role: "Back-end Developer",
+    photo: Shamarka,
+    description:
+      "The backbone of its technical foundation. His Knowledge and dedication have been vital in shaping Learnly into a dynamic and efficient platform for educational growth.",
+    email: "mohamed.shamarka30@gmail.com",
+    linkedIn: "https://www.linkedin.com/in/mohamed-shamarka",
+    github: "https://wwww.github.com/Shamarka",
+  },
+  {
+    name: "Mohammad Helaly",
+    role: "Full Stack Developer",
+    photo: mohammadHelaly,
+    description:
+      "The creative mind behind our platform. He combined his technical frontend and backend expertise to build Learnly and empower students and instructors globally. Passionate about quality education, he's committed to making learning engaging for all.",
+    email: "mohammad.helaly@gmail.com",
+    linkedIn: "https://www.linkedin.com/in/mohammadhelaly/",
+    github: "https://wwww.github.com/MohammadHelaly",
+  },
+  {
+    name: "Mohammad Helaly",
+    role: "Full Stack Developer",
+    photo: mohammadHelaly,
+    description:
+      "The creative mind behind our platform. He combined his technical frontend and backend expertise to build Learnly and empower students and instructors globally. Passionate about quality education, he's committed to making learning engaging for all.",
+    email: "mohammad.helaly@gmail.com",
+    linkedIn: "https://www.linkedin.com/in/mohammadhelaly/",
+    github: "https://wwww.github.com/MohammadHelaly",
+  },
+  {
+    name: "Mohammad Helaly",
+    role: "Full Stack Developer",
+    photo: mohammadHelaly,
+    description:
+      "The creative mind behind our platform. He combined his technical frontend and backend expertise to build Learnly and empower students and instructors globally. Passionate about quality education, he's committed to making learning engaging for all.",
+    email: "mohammad.helaly@gmail.com",
+    linkedIn: "https://www.linkedin.com/in/mohammadhelaly/",
+    github: "https://wwww.github.com/MohammadHelaly",
+  },
 ];
 
 export default teamMembers;
