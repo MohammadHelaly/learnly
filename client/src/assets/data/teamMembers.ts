554e5085ef9ceb5bc76dad0e33083705df7b1151
MohammadHelaly <mohammad.helaly@gmail.com>
2024-06-20 02:57:45 +0300

    Update team section

--- client/src/assets/data/teamMembers.ts
+++ client/src/assets/data/teamMembers.ts
@@ -1,7 +1,10 @@
 import mohammadHelaly from "../images/mohammad-helaly.jpg";
-import moustafaDaebis from "../images/moustafaDaebis.jpg";
-import Shamarka from "../images/shamarka.jpg";
-import omarShehab from "../images/shehab.jpg";
+import moustafaDaebis from "../images/moustafa-daebis.jpg";
+import mohamedShamarka from "../images/mohamed-shamarka.jpg";
+import omarShehab from "../images/omar-shehab.jpg";
+import youssefWalid from "../images/youssef-walid.jpg";
+import abdelrahmanElAttar from "../images/abdelrahman-elattar.jpg";
+
 const teamMembers: TeamMember[] = [
 	{
 		name: "Mohammad Helaly",
@@ -26,9 +29,9 @@ const teamMembers: TeamMember[] = [
 	{
 		name: "Mohamed Shamarka",
 		role: "Back End Developer",
-		photo: Shamarka,
+		photo: mohamedShamarka,
 		description:
-			"The backbone of its technical foundation. His Knowledge, Passion and dedication have been vital in shaping Learnly into a dynamic and efficient platform for educational growth while offering a seamless experience for its users.",
+			"The backbone of our platforms technical foundation. His Knowledge, Passion and dedication have been vital in shaping Learnly into a dynamic and efficient platform for educational growth while offering a seamless experience for its users.",
 		email: "mohamed.shamarka30@gmail.com",
 		linkedIn: "https://www.linkedin.com/in/mohamed-shamarka",
 		github: "https://github.com/Shamarka",
@@ -38,30 +41,30 @@ const teamMembers: TeamMember[] = [
 		role: "Front End Developer",
 		photo: omarShehab,
 		description:
-			"As a front-end specialist, he played a key role in creating an intuitive and engaging online learning environment. With a focus on user experience and responsive design, he developed and implemented interactive features that enhance the educational experience for users.",
+			"As a frontend specialist, he played a key role in creating an intuitive and engaging online learning environment. With a focus on user experience and responsive design, he developed and implemented interactive features that enhance the educational experience for users.",
 		email: "omar.shehab121@gmail.com",
 		linkedIn: "https://www.linkedin.com/in/omar-shehab-0872472a3",
 		github: "https://wwww.github.com/omarhshehab7",
 	},
 	{
-		name: "Mohammad Helaly",
-		role: "Full Stack Developer",
-		photo: mohammadHelaly,
+		name: "Youssef Walid",
+		role: "Front End Developer",
+		photo: youssefWalid,
 		description:
-			"The creative mind behind our platform. He combined his technical frontend and backend expertise to build Learnly and empower students and instructors globally. Passionate about quality education, he's committed to making learning engaging for all.",
-		email: "mohammad.helaly@gmail.com",
-		linkedIn: "https://www.linkedin.com/in/mohammadhelaly/",
-		github: "https://wwww.github.com/MohammadHelaly",
+			"A frontend developer with a passion for creating engaging and user-friendly interfaces. He played a key role in designing and developing Learnly's platform, ensuring a seamless user experience for students and instructors.",
+		email: "usefwalid718@gmail.com",
+		linkedIn: "https://www.linkedin.com/in/youssef-walid-92b690269/",
+		github: "https://wwww.github.com/usefwalid",
 	},
 	{
-		name: "Mohammad Helaly",
-		role: "Full Stack Developer",
-		photo: mohammadHelaly,
+		name: "Abdelrahman El Attar",
+		role: "Back End Developer",
+		photo: abdelrahmanElAttar,
 		description:
-			"The creative mind behind our platform. He combined his technical frontend and backend expertise to build Learnly and empower students and instructors globally. Passionate about quality education, he's committed to making learning engaging for all.",
+			"An experienced backend developer who contributed to the development of Learnly's platform. He leveraged his technical expertise to build a robust and scalable backend that supports the platform's functionality and performance.",
 		email: "mohammad.helaly@gmail.com",
-		linkedIn: "https://www.linkedin.com/in/mohammadhelaly/",
-		github: "https://wwww.github.com/MohammadHelaly",
+		linkedIn: "https://www.linkedin.com/in/abdelrahmanattar/",
+		github: "https://wwww.github.com/abdelrahmanattar",
 	},
 ];
 
